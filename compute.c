3b521995aed844e94e791ba5c41dfba912bcebf4
JOSHUA S. M. TAING <JOSTAI2003@GMAIL.COM>
2023-11-25 17:12:14 -0500

    Edited compute.c, compute.h, vector.h -- deleted compute.cu

--- compute.c
+++ compute.c
@@ -10,73 +10,25 @@ Max Mazal
 #include "vector.h"
 #include "config.h"
 
-//Device variables
-vector3* dPos;
-vector3* dVel;
-double* dMass;
-vector3* dAccels;
+//initAccels: Initializes the acceleration matrix which is NUMENTITIES squared in size
+//Parameters: vector3 *accels, vector3 *vals, int numEntities
+//Returns: none
+//Notes: i'm thinking we call this first in main, then compute; why am i splitting it up? works like that in my brain
+__global__ void initAccels(vector3 *accels, vector3 *vals, int numEntities){
+	int idx = threadIdx.x + blockIdx.x * blockDim.x;
+	if (idx < numEntities) {
+		accels[idx] = &values[idx*numEntities];
+    }
+}
 
 //compute: Updates the positions and locations of the objects in the system based on gravity.
 //Parameters: None
 //Returns: None
 //Side Effect: Modifies the hPos and hVel arrays with the new positions and accelerations after 1 INTERVAL
-void compute(){
+__global__ void compute(double *d_mass, vector3 *d_hPos, vector3 *d_hVel){
+	//You probably noticed I removed memory stuff. Think it is better to do it within nbody.c, in the main function.
 
-	//Code to allocate the device variables, allocates at the very start of the compute method before any computing is done
-	cudaMalloc((void**)&dPos, sizeof(vector3) * NUMENTITIES);
-	cudaMalloc((void**)&dVel, sizeof(vector3) * NUMENTITIES);
-	cudaMalloc((void**)&dMass, sizeof(double) * NUMENTITIES);
-	cudaMalloc((void**)&dAccels, sizeof(vector3) * NUMENTITIES * NUMENTITIES);
+    //First compute the pairwise accelerations.  Effect is on the first argument.
 
-	//Copies the data from the CPU to the GPU to ensure they both have the same initial data before computing
-	cudaMemcpy(dPos, hPos, sizeof(vector3) * NUMENTITIES, cudaMemcpyHostToDevice);
-	cudaMemcpy(dVel, hVel, sizeof(vector3) * NUMENTITIES, cudaMemcpyHostToDevice);
-	cudaMemcpy(dMass, mass, sizeof(double) * NUMENTITIES, cudaMemcpyHostToDevice);
-
-
-	//make an acceleration matrix which is NUMENTITIES squared in size;
-	int i,j,k;
-	vector3* values=(vector3*)malloc(sizeof(vector3)*NUMENTITIES*NUMENTITIES);
-	vector3** accels=(vector3**)malloc(sizeof(vector3*)*NUMENTITIES);
-	for (i=0;i<NUMENTITIES;i++)
-		accels[i]=&values[i*NUMENTITIES];
-	//first compute the pairwise accelerations.  Effect is on the first argument.
-	for (i=0;i<NUMENTITIES;i++){
-		for (j=0;j<NUMENTITIES;j++){
-			if (i==j) {
-				FILL_VECTOR(accels[i][j],0,0,0);
-			}
-			else{
-				vector3 distance;
-				for (k=0;k<3;k++) distance[k]=hPos[i][k]-hPos[j][k];
-				double magnitude_sq=distance[0]*distance[0]+distance[1]*distance[1]+distance[2]*distance[2];
-				double magnitude=sqrt(magnitude_sq);
-				double accelmag=-1*GRAV_CONSTANT*mass[j]/magnitude_sq;
-				FILL_VECTOR(accels[i][j],accelmag*distance[0]/magnitude,accelmag*distance[1]/magnitude,accelmag*distance[2]/magnitude);
-			}
-		}
-	}
-	//sum up the rows of our matrix to get effect on each entity, then update velocity and position.
-	for (i=0;i<NUMENTITIES;i++){
-		vector3 accel_sum={0,0,0};
-		for (j=0;j<NUMENTITIES;j++){
-			for (k=0;k<3;k++)
-				accel_sum[k]+=accels[i][j][k];
-		}
-		//compute the new velocity based on the acceleration and time interval
-		//compute the new position based on the velocity and time interval
-		for (k=0;k<3;k++){
-			hVel[i][k]+=accel_sum[k]*INTERVAL;
-			hPos[i][k]+=hVel[i][k]*INTERVAL;
-		}
-	}
-	free(accels);
-	free(values);
-
-	//Frees the memory after computing
-	cudaFree(dPos);
-	cudaFree(dVel);
-	cudaFree(dMass);
-	cudaFree(dAccels);
-
+    //Sum up the rows of our matrix to get effect on each entity, then update velocity and position.
 }
--- compute.h
+++ compute.h
@@ -1,1 +1,2 @@
-void compute();
+__global__ void initAccels(vector3 *accels, vector3 *vals, int numEntities);
+__global__ void compute(double *d_mass, vector3 *d_hPos, vector3 *d_hVel);
