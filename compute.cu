a9001bd0fa2d9f74123e834c4a54d6cafb08d145
Max VBox <maxmazal@udel.edu>
2023-12-01 13:00:18 -0500

    compute.cu reset

--- compute.cu
+++ compute.cu
@@ -14,54 +14,51 @@ Max Mazal
 //Parameters: vector3 *accels, vector3 *vals, int numEntities
 //Returns: none
 //Notes: i'm thinking we call this first in main, then compute; why am i splitting it up? works like that in my brain
-__global__ void initAccels(vector3 accels, vector3 *vals, int numEntities){
-	int idx = threadIdx.x + blockIdx.x * blockDim.x;
-	if (idx < numEntities) {
-		accels[idx] = &vals[idx*numEntities];
-    }
-}
+
 
 //compute: Updates the positions and locations of the objects in the system based on gravity.
 //Parameters: None
 //Returns: None
 //Side Effect: Modifies the hPos and hVel arrays with the new positions and accelerations after 1 INTERVAL
-__global__ void compute(vector3 *accels, vector3 *accel_sum, vector3 *hVel, vector3 *hPos, double *mass){
-	//You probably noticed I removed memory stuff. Think it is better to do it within nbody.c, in the main function.
-    //First compute the pairwise accelerations.  Effect is on the first argument.
-	int a = blockDim.y * blockIdx.y + threadIdx.y;
-	int b = blockDim.x * blockIdx.x + threadIdx.x;
 
-	if (a < NUMENTITIES && b < NUMENTITIES) {
-		if (a == b) {
-			FILL_VECTOR(accels[a][b], 0, 0, 0);
-		}
-		else {
-			vector3 distance;
-			for (int k = 0; k < 3; k++) {
-				distance[k] = hPos[b][k] - hPos[a][k]; //changed from "hPos[a][k] - hPos[a][k]" to current 
+__global__ void compute(){
+	//make an acceleration matrix which is NUMENTITIES squared in size;
+	int i,j,k;
+	vector3* values=(vector3*)malloc(sizeof(vector3)*NUMENTITIES*NUMENTITIES);
+	vector3** accels=(vector3**)malloc(sizeof(vector3*)*NUMENTITIES);
+	for (i=0;i<NUMENTITIES;i++)
+		accels[i]=&values[i*NUMENTITIES];
+	//first compute the pairwise accelerations.  Effect is on the first argument.
+	for (i=0;i<NUMENTITIES;i++){
+		for (j=0;j<NUMENTITIES;j++){
+			if (i==j) {
+				FILL_VECTOR(accels[i][j],0,0,0);
 			}
-			double magnitude_sq = distance[0] * distance[0] + distance[1] * distance[1] + distance[2] * distance[2];
-			double magnitude = sqrt(magnitude_sq);
-			double accelmag = -1 * GRAV_CONSTANT * mass[b] / magnitude_sq; //changed from mass j to mass b
-			FILL_VECTOR(accels[a][b], accelmag * distance[0] / magnitude, accelmag * distance[1] / magnitude, accelmag * distance[2] / magnitude);
+			else{
+				vector3 distance;
+				for (k=0;k<3;k++) distance[k]=hPos[i][k]-hPos[j][k];
+				double magnitude_sq=distance[0]*distance[0]+distance[1]*distance[1]+distance[2]*distance[2];
+				double magnitude=sqrt(magnitude_sq);
+				double accelmag=-1*GRAV_CONSTANT*mass[j]/magnitude_sq;
+				FILL_VECTOR(accels[i][j],accelmag*distance[0]/magnitude,accelmag*distance[1]/magnitude,accelmag*distance[2]/magnitude);
+			}
 		}
 	}
-
-	//synchronizes the threads before procceeding
-	__syncthreads();
-
-    //Sum up the rows of our matrix to get effect on each entity.
-	int c = blockIdx.x * blockDim.x + threadIdx.x;
-	int d = blockIdx.y;
-
-	if (c < NUMENTITIES) {
-		FILL_VECTOR(accel_sum[c], 0, 0, 0);
-		for (int k = 0; k < NUMENTITIES; k++) {
-			accel_sum[c][d] += accels[c][k][d];
+	//sum up the rows of our matrix to get effect on each entity, then update velocity and position.
+	for (i=0;i<NUMENTITIES;i++){
+		vector3 accel_sum={0,0,0};
+		for (j=0;j<NUMENTITIES;j++){
+			for (k=0;k<3;k++)
+				accel_sum[k]+=accels[i][j][k];
 		}
-		//Then update velocity and position.
-		hVel[c][d] += accel_sum[c][d];
-		hPos[c][d] += hVel[c][d] * INTERVAL;
+		//compute the new velocity based on the acceleration and time interval
+		//compute the new position based on the velocity and time interval
+		for (k=0;k<3;k++){
+			hVel[i][k]+=accel_sum[k]*INTERVAL;
+			hPos[i][k]+=hVel[i][k]*INTERVAL;
+		}
 	}
+	free(accels);
+	free(values);
 }
 
