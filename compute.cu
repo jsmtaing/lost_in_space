4565dfca8400d4fc103e89a464c291e36a66feb3
maxmazal <maxmazal@udel.edu>
2023-12-03 18:08:04 -0500

    debug

--- compute.cu
+++ compute.cu
@@ -109,13 +109,13 @@ __global__ void compute(vector3 *d_accels, vector3 *d_accel_sum, vector3 *d_hVel
 		// 	hPos[i][k]+=hVel[i][k]*INTERVAL;
 		// }
 	
-	printf("Debug: Thread (%d, %d) - Mass: %f\n", threadIdx.y, threadIdx.x, sharedMass[row]);
+	fprintf("Debug: Thread (%d, %d) - Mass: %f\n", threadIdx.y, threadIdx.x, sharedMass[row]);
 
-    printf("Debug: Thread (%d, %d) - Pos[%d]: %f, %f, %f\n", threadIdx.y, threadIdx.x, row, sharedPos[row][0], sharedPos[row][1], sharedPos[row][2]);
+    fprintf("Debug: Thread (%d, %d) - Pos[%d]: %f, %f, %f\n", threadIdx.y, threadIdx.x, row, sharedPos[row][0], sharedPos[row][1], sharedPos[row][2]);
 
-    printf("Debug: Thread (%d, %d) - Vel[%d]: %f, %f, %f\n", threadIdx.y, threadIdx.x, row, sharedVel[row][0], sharedVel[row][1], sharedVel[row][2]);
+    fprintf("Debug: Thread (%d, %d) - Vel[%d]: %f, %f, %f\n", threadIdx.y, threadIdx.x, row, sharedVel[row][0], sharedVel[row][1], sharedVel[row][2]);
 
-    printf("Debug: Thread (%d, %d) - AccelSum[%d]: %f, %f, %f\n", threadIdx.y, threadIdx.x, row, accelSum[0], accelSum[1], accelSum[2]);
+    fprintf("Debug: Thread (%d, %d) - AccelSum[%d]: %f, %f, %f\n", threadIdx.y, threadIdx.x, row, accelSum[0], accelSum[1], accelSum[2]);
 
 
 	free(accels);
