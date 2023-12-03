6300957f51d798d92a36b85246f736ec70a4afb9
maxmazal <maxmazal@udel.edu>
2023-12-03 14:57:36 -0500

    testing 2:57pm

--- compute.cu
+++ compute.cu
@@ -15,10 +15,11 @@ Max Mazal
 //Returns: None
 //Side Effect: Modifies the hPos and hVel arrays with the new positions and accelerations after 1 INTERVAL
 __global__ void compute(double *d_mass, vector3 *d_hPos, vector3 *d_hVel){
+	//make an acceleration matrix which is NUMENTITIES squared in size;
+	int i,j,k;
 
 	//thread / block indices
-	//Max 12.3.23 12pm
-	//swapped variables row and column (row corresponds to blockIdx.x and vice versa)
+	//Max 12.3.23 12pm; swapped variables row and column (row corresponds to blockIdx.x and vice versa)
 	int row = blockIdx.x * blockDim.x + threadIdx.x;
     int col = blockIdx.y * blockDim.y + threadIdx.y;
     
@@ -27,52 +28,52 @@ __global__ void compute(double *d_mass, vector3 *d_hPos, vector3 *d_hVel){
 	__shared__ double sharedMass[BLOCK_SIZE];
 	__shared__ vector3 sharedPos[BLOCK_SIZE];
     __shared__ vector3 sharedVel[BLOCK_SIZE];
-	__shared__ vector3 accel_sum[BLOCK_SIZE];
 
 	//Max 12.3.23 12pm
 	//these arrays load each thread's mass, position, and velocity data into the shared memory
-	sharedMass[threadIdx.x] = d_mass[col];
-    sharedPos[threadIdx.x] = d_hPos[col];
-    sharedVel[threadIdx.x] = d_hVel[col];
+	sharedMass[threadIdx.x] = d_mass[j];
+    sharedPos[threadIdx.x] = d_hPos[j];
+    sharedVel[threadIdx.x] = d_hVel[j];
 
 	//Max 12.3.23 12pm
 	//syncs the threads to ensure each block finished loading data into shared memory before procceeding with computation
 	__syncthreads();
 
-    
-
-	accel_sum[threadIdx.x] = {0, 0, 0};
-
-	__syncthreads();
-
-
+	vector3* values=(vector3*)malloc(sizeof(vector3)*NUMENTITIES*NUMENTITIES);
+	vector3** accels=(vector3**)malloc(sizeof(vector3*)*NUMENTITIES);
+	for (i=0;i<NUMENTITIES;i++)
+		accels[i]=&values[i*NUMENTITIES];
 	//first compute the pairwise accelerations.  Effect is on the first argument.
-	for (int j = 0; j < NUMENTITIES; j++) {
-        if (row != j) {
-            vector3 distance;
-            for (int k = 0; k < 3; k++)
-                distance[k] = sharedPos[threadIdx.x][k] - d_hPos[j][k];
-
-            double magnitude_sq = distance[0] * distance[0] + distance[1] * distance[1] + distance[2] * distance[2];
-            double magnitude = sqrt(magnitude_sq);
-            double accelmag = -1 * GRAV_CONSTANT * sharedMass[threadIdx.x] / magnitude_sq;
-
-            for (int k = 0; k < 3; k++)
-                atomicAdd(&accel_sum[threadIdx.x][k], accelmag * distance[k] / magnitude);
+	for (i=0;i<NUMENTITIES;i++){
+		for (j=0;j<NUMENTITIES;j++){
+			if (i==j) {
+				FILL_VECTOR(accels[i][j],0,0,0);
+			}
+			else{
+				vector3 distance;
+				for (k=0;k<3;k++) distance[k]=hPos[i][k]-hPos[j][k];
+				double magnitude_sq=distance[0]*distance[0]+distance[1]*distance[1]+distance[2]*distance[2];
+				double magnitude=sqrt(magnitude_sq);
+				double accelmag=-1*GRAV_CONSTANT*mass[j]/magnitude_sq;
+				FILL_VECTOR(accels[i][j],accelmag*distance[0]/magnitude,accelmag*distance[1]/magnitude,accelmag*distance[2]/magnitude);
+			}
 		}
-    }
-
-	__syncthreads();
-
-	
-	 for (int k = 0; k < 3; k++) {
-        sharedVel[threadIdx.x][k] += accel_sum[k] * INTERVAL;
-        sharedPos[threadIdx.x][k] += sharedVel[threadIdx.x][k] * INTERVAL;
-    }
-
-    __syncthreads();
-
-    d_hVel[col] = sharedVel[threadIdx.x];
-    d_hPos[col] = sharedPos[threadIdx.x];
+	}
+	//sum up the rows of our matrix to get effect on each entity, then update velocity and position.
+	for (i=0;i<NUMENTITIES;i++){
+		vector3 accel_sum={0,0,0};
+		for (j=0;j<NUMENTITIES;j++){
+			for (k=0;k<3;k++)
+				accel_sum[k]+=accels[i][j][k];
+		}
+		//compute the new velocity based on the acceleration and time interval
+		//compute the new position based on the velocity and time interval
+		for (k=0;k<3;k++){
+			hVel[i][k]+=accel_sum[k]*INTERVAL;
+			hPos[i][k]+=hVel[i][k]*INTERVAL;
+		}
+	}
+	free(accels);
+	free(values);
 }
 
