c5f18c577324ba7db8763681850e921269e0deb5
maxmazal <maxmazal@udel.edu>
2023-12-03 14:29:46 -0500

    test

--- compute.cu
+++ compute.cu
@@ -62,55 +62,22 @@ __global__ void compute(double *d_mass, vector3 *d_hPos, vector3 *d_hVel){
 			}
 		}
 	}
-	// //sum up the rows of our matrix to get effect on each entity, then update velocity and position.
-	// for (i=0;i<NUMENTITIES;i++){
-	// 	vector3 accel_sum={0,0,0};
-	// 	for (j=0;j<NUMENTITIES;j++){
-	// 		for (k=0;k<3;k++)
-	// 			accel_sum[k] += accels[i * NUMENTITIES + j][k];; //changed this from accel_sum[k]+=accels[i][j][k] 
-	// 	}
-	// 	//compute the new velocity based on the acceleration and time interval
-	// 	//compute the new position based on the velocity and time interval
-	// 	for (k=0;k<3;k++){
-	// 		sharedVel[threadIdx.x][k] += accel_sum[k] * INTERVAL;
-    //         sharedPos[threadIdx.x][k] += sharedVel[threadIdx.x][k] * INTERVAL;
-	// 		// hVel[i][k]+=accel_sum[k]*INTERVAL;
-	// 		// hPos[i][k]+=hVel[i][k]*INTERVAL;
-	// 	}
-	// }
 
-	// //Max 12.3.23 1pm
-	// //syncs to ensure all threads have finished
-    // __syncthreads();
-
-	vector3 accel_sum = {0, 0, 0};
-    for (int j = 0; j < NUMENTITIES; j++) {
-        for (int k = 0; k < 3; k++)
-            accel_sum[k] += accels[row][j][k];
-    }
-
-
-	//Max 12.3.23 1pm
-    //updates the shared memory with the new velocity and position
-    for (k = 0; k < 3; k++) {
-        sharedVel[threadIdx.x][k] += accel_sum[k] * INTERVAL;
-        sharedPos[threadIdx.x][k] += sharedVel[threadIdx.x][k] * INTERVAL;
-    }
-
-	//Max 12.3.23 1pm
-    __syncthreads();
-
-	//Max 12.3.23 1pm
-    //results go to global memory
-    if (threadIdx.x == 0) {
-        for (i = 0; i < NUMENTITIES; i++) {
-            //
-            d_hVel[col] = sharedVel[i];
-            d_hPos[col] = sharedPos[i];
-        }
-    }
-
-
+	
+	//sum up the rows of our matrix to get effect on each entity, then update velocity and position.
+	for (i=0;i<NUMENTITIES;i++){
+		vector3 accel_sum={0,0,0};
+		for (j=0;j<NUMENTITIES;j++){
+			for (k=0;k<3;k++)
+				accel_sum[k]+=accels[i][j][k];
+		}
+		//compute the new velocity based on the acceleration and time interval
+		//compute the new position based on the velocity and time interval
+		for (k=0;k<3;k++){
+			hVel[i][k]+=accel_sum[k]*INTERVAL;
+			hPos[i][k]+=hVel[i][k]*INTERVAL;
+		}
+	}
 	free(accels);
 	free(values);
 }
