de7bcd29ee77220f17c51c1eca1a3020568bbbb2
maxmazal <maxmazal@udel.edu>
2023-12-10 18:56:53 -0500

    checking twice for cuda errors

--- compute.cu
+++ compute.cu
@@ -79,6 +79,10 @@ void compute() {
         printf("Error: %s\n", cudaGetErrorString(err));
 
     sum_update<<<gridDim, blockDim>>>(d_hVel, d_hPos, d_accels);
+    cudaError_t err = cudaGetLastError();
+    if (err != cudaSuccess) 
+        printf("Error: %s\n", cudaGetErrorString(err));
+        
     cudaDeviceSynchronize();
 
     cudaMemcpy(hPos, d_hPos, sizeof(vector3)*NUMENTITIES, cudaMemcpyDeviceToHost);
